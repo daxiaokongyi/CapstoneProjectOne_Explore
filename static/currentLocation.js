4c62aa81240c343a0637a6858d5528a2d9f7a835
JJ <jasonjinhang@hotmail.com>
2021-02-15 16:09:51 -0800

    fixing axios.post

--- static/currentLocation.js
+++ static/currentLocation.js
@@ -1,10 +1,10 @@
 let currentLat = localStorage.getItem('latitude');
 let currentLong = localStorage.getItem('longitude');
-const axios = require('axios');
 
 // console.log(currentLat, currentLong);
-async function currentLocation() {
-    let payload = {latitude: currentLat, longitude: currentLong};
+async function currentLocation(evt) {
+    evt.preventDefault();
+    let payload = {"latitude": currentLat, "longitude": currentLong};
     let res = await axios.post('/api/location', payload);
     let data = res.data;
     console.log(data);
